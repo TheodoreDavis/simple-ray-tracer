224b037dbaaa4c8cdebd625e94834f9a6cbba1f8
sgriffen <griffensean@gmail.com>
2021-09-10 01:38:19 -0500

    - Updating inc/cmpfloat.h to use std::numeric_limits<float>::epsilon() instead of c FLT_EPSILON - Cleaning test/test-cmpfloat.cpp

--- inc/cmpfloat.h
+++ inc/cmpfloat.h
@@ -1,10 +1,10 @@
-#include <cfloat>
+#include <limits>
 #include <cmath>
 
 //Collection of macros to handle comparisons between two floating point numbers
-#define CMPFLOAT_EQUAL(a, b) (std::abs(a - b) <= FLT_EPSILON)
-#define CMPFLOAT_NEQUAL(a, b) (! CMPFLOAT_EQUAL(a, b))
-#define CMPFLOAT_GREATER(a, b) ((a - b) > FLT_EPSILON)
-#define CMPFLOAT_LESS(a, b) ((a - b) < (0 - FLT_EPSILON))
+#define CMPFLOAT_EQUAL(a, b) (std::abs(a - b) <= std::numeric_limits<float>::epsilon())
+#define CMPFLOAT_NEQUAL(a, b) (!CMPFLOAT_EQUAL(a, b))
+#define CMPFLOAT_GREATER(a, b) ((a - b) > std::numeric_limits<float>::epsilon())
+#define CMPFLOAT_LESS(a, b) ((a - b) < (0 - std::numeric_limits<float>::epsilon()))
 #define CMPFLOAT_EGREATER(a, b) (CMPFLOAT_EQUAL(a, b) || CMPFLOAT_GREATER(a, b))
 #define CMPFLOAT_ELESS(a, b) (CMPFLOAT_EQUAL(a, b) || CMPFLOAT_LESS(a, b))
