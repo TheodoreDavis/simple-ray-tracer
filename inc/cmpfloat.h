39baf2a48cdc983b22f72770bcb8262615390f23
sgriffen <griffensean@gmail.com>
2021-09-10 20:45:09 -0500

    - Changing cmpfloat macros 'EGREATER' and 'ELESS' to use '!(LESS)' and '!(GREATER)' macros respectively. 	- Previous implementation could cause compiler errors?

--- inc/cmpfloat.h
+++ inc/cmpfloat.h
@@ -3,8 +3,8 @@
 
 //Collection of macros to handle comparisons between two floating point numbers
 #define CMPFLOAT_EQUAL(a, b) (std::abs(a - b) <= std::numeric_limits<float>::epsilon())
-#define CMPFLOAT_NEQUAL(a, b) (!CMPFLOAT_EQUAL(a, b))
+#define CMPFLOAT_NEQUAL(a, b) (!(CMPFLOAT_EQUAL(a, b)))
 #define CMPFLOAT_GREATER(a, b) ((a - b) > std::numeric_limits<float>::epsilon())
 #define CMPFLOAT_LESS(a, b) ((a - b) < (0 - std::numeric_limits<float>::epsilon()))
-#define CMPFLOAT_EGREATER(a, b) (CMPFLOAT_EQUAL(a, b) || CMPFLOAT_GREATER(a, b))
-#define CMPFLOAT_ELESS(a, b) (CMPFLOAT_EQUAL(a, b) || CMPFLOAT_LESS(a, b))
+#define CMPFLOAT_EGREATER(a, b) (!(CMPFLOAT_LESS(a, b)))
+#define CMPFLOAT_ELESS(a, b) (!(CMPFLOAT_GREATER(a, b)))
